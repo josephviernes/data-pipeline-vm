402f3602260fc77f384929632a7d3e6655f71f19
joseph8781 <josephnviernes@gmail.com>
2025-05-23 13:18:50 +0800

    Made the scraper script modular

--- docs/installations.md
+++ docs/installations.md
@@ -2,3 +2,6 @@ Required Libraries:
 
 pip install beautifulsoup4 requests urllib3 google-cloud-storage google-cloud-bigquery pyspark
 
+ENVIRONMENT VARIABLES:
+
+export GOOGLE_APPLICATION_CREDENTIALS="path/to/json"
--- scrape/scraper.py
+++ scrape/scraper.py
@@ -4,52 +4,33 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import urllib3
-import time
-from datetime import datetime, timedelta
-import calendar
+from datetime import datetime
 import csv
 import io
 from google.cloud import storage
 
-# Disable SSL warnings (since we're ignoring SSL verification)
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# get the month
-now = datetime.now()
-current_month = now.month
+def fetch_data(url):
+    try:
+        # Fetch the webpage with SSL verification disabled
+        response = requests.get(url, verify=False)
+        response.raise_for_status()  # Raise an error if request fails (e.g., 404, 500)
+        return bs(response.content)
 
-# convert the month number to month name, get the current year and number
-month = now.strftime("%B")
-year = now.year
-day = now.day
-hour = f"{now.hour:02d}"
-minute = f"{now.minute:02d}"
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
 
-# This will create a list that will store the extracted data
-earthquake_data = []
+def parse_data(soup):
 
+    # This will create a list that will store the extracted data
+    earthquake_data = []
 
-# PHIVOLCS Earthquake URL
-url_link = "https://earthquake.phivolcs.dost.gov.ph/" 
-# f"https://earthquake.phivolcs.dost.gov.ph/EQLatest-Monthly/{year}/{year}_{month}.html"
-
-try:
-    # Fetch the webpage with SSL verification disabled
-    response = requests.get(url_link, verify=False)
-    response.raise_for_status()  # Raise an error if request fails (e.g., 404, 500)
-    soup = bs(response.content)
-
-except requests.exceptions.RequestException as e:
-    print(f"Error fetching data: {e}")
-
-if soup:
     # Extract the table containing the earthquake data, skips the first 2 tables [0:1]
     table = soup.body.find_all("table", class_="MsoNormalTable")[2]
 
 
     # Refine the HTML data (only data with <tr> tag, also skips the header[0:1]
     rows = table.find_all("tr")[2:]
-
 
     # This will extract the data contained in <td> tags that is within the <tr> tag. The data in the <td> tag is then stored in the earthquake_data list
     for row in rows:
@@ -64,10 +45,11 @@ if soup:
 
         earthquake_data.append([date_time, latitude, longitude, depth, magnitude, relative_location])
 
+    return earthquake_data
 
 
 
-def upload_to_gcs(bucket_name, destination_blob_name):
+def upload_to_gcs(earthquake_data, bucket_name, destination_blob_name):
     # Create a storage client
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
@@ -90,7 +72,33 @@ def upload_to_gcs(bucket_name, destination_blob_name):
     blob.upload_from_string(csv_buffer.getvalue(), content_type='text/csv')
     print(f"List uploaded as CSV to gs://{bucket_name}/{destination_blob_name}")
 
-upload_to_gcs(
-    bucket_name="phivolcs_earthquake_data",
-    destination_blob_name=f"dailies/{year}_{month}_{day}_{hour}_{minute}_earthquake_data.csv"
-)
+
+
+def main():
+
+    # Disable SSL warnings (since we're ignoring SSL verification)
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    # get the date, convert the month number to month name, get the current year, hour and minute
+    now = datetime.now()
+    month = now.strftime("%B")
+    year = now.year
+    day = now.day
+    hour = f"{now.hour:02d}"
+    minute = f"{now.minute:02d}"
+
+    # PHIVOLCS Earthquake URL
+    url = "https://earthquake.phivolcs.dost.gov.ph/" 
+    # f"https://earthquake.phivolcs.dost.gov.ph/EQLatest-Monthly/{year}/{year}_{month}.html"
+
+
+    upload_to_gcs(
+        earthquake_data=parse_data(fetch_data(url)),
+        bucket_name="phivolcs_earthquake_data",
+        destination_blob_name=f"dailies/{year}_{month}_{day}_{hour}_{minute}_earthquake_data.csv"         
+    )
+
+
+if __name__ == "__main__":
+    main()
+
