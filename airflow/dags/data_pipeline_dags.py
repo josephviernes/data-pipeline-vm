e80c75e1ff00aebb39b47774fc90b95bc50528e7
joseph8781 <josephnviernes@gmail.com>
2025-06-05 12:01:07 +0800

    edit dag

--- airflow/dags/data_pipeline_dags.py
+++ airflow/dags/data_pipeline_dags.py
@@ -1,14 +1,19 @@
-from airflow.decorators import task, dag
+from airflow.sdk import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
+from datetime import datetime, timedelta
 
-from datetime import datetime
+default_args = {
+    'owner': 'jnv'
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5)
+}
 
-@dag(start_date=datetime(2025, 6, 1), schedule_interval='daily', catchup=False)
 
-def docker_dag():
+with DAG(
+    default_args
+    description="Phivolcs earthquake data pipeline"
+    tags=["earthquake_data_pipeline"]
+    catchup=False,
+    schedule="0 6,18 * * *"
 
-    @task()
-    t1 = DockerOperator(
-        task_id='t1'
-        image=''
-    )
+) as dag:
