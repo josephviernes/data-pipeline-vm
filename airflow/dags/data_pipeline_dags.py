3817fb0b81ea25fb2e6b11c98363a50eb53d1a00
joseph8781 <josephnviernes@gmail.com>
2025-06-14 21:02:12 +0800

    fixed issues on task; mounts from volume

--- airflow/dags/data_pipeline_dags.py
+++ airflow/dags/data_pipeline_dags.py
@@ -1,16 +1,13 @@
-import os
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime, timedelta
-
-google_creds = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+from docker.types import Mount
 
 default_args = {
     'owner': 'jnv',
     'retries': 2,
     'retry_delay': timedelta(minutes=5),
 }
-
 
 with DAG(
     default_args=default_args,
@@ -19,7 +16,6 @@ with DAG(
     catchup=False,
     schedule=None,
     start_date=datetime(2025, 6, 10),
-
 ) as dag:
 
     t1 = DockerOperator(
@@ -27,35 +23,41 @@ with DAG(
         image='earthquake_data_web_scraper:v1.4',
         docker_url='unix://var/run/docker.sock',
         network_mode='bridge',
-        environment={"GOOGLE_APPLICATION_CREDENTIALS": google_creds},
+        mounts=[
+            Mount(source="/var/run", target="/var/run", type="bind"),
+            Mount(source="/home/joseph/Documents/dez_final_project/gsa", target="/gsa", type="bind", read_only=True)
+        ],
+        environment={"GOOGLE_APPLICATION_CREDENTIALS": "/gsa/finalproject-456408-a18af71e91f6.json"},
         command='python3 scraper.py',
         auto_remove=True,
-
-        
     )
 
     t2 = DockerOperator(
         task_id="processor",
         image='earthquake_data_processor:v1.6',
         docker_url='unix://var/run/docker.sock',
-        environment={"GOOGLE_APPLICATION_CREDENTIALS": google_creds},
         network_mode='bridge',
+        mounts=[
+            Mount(source="/var/run", target="/var/run", type="bind"),
+            Mount(source="/home/joseph/Documents/dez_final_project/gsa", target="/gsa", type="bind", read_only=True)
+        ],
+        environment={"GOOGLE_APPLICATION_CREDENTIALS": "/gsa/finalproject-456408-a18af71e91f6.json"},
         command='python3 processor.py',
         auto_remove=True,
-
-        
     )
-
 
     t3 = DockerOperator(
         task_id="merger",
         image='earthquake_data_merger:v1.4',
         docker_url='unix://var/run/docker.sock',
         network_mode='bridge',
-        environment={"GOOGLE_APPLICATION_CREDENTIALS": google_creds},
+        mounts=[
+            Mount(source="/var/run", target="/var/run", type="bind"),
+            Mount(source="/home/joseph/Documents/dez_final_project/gsa", target="/gsa", type="bind", read_only=True)
+        ],
+        environment={"GOOGLE_APPLICATION_CREDENTIALS": "/gsa/finalproject-456408-a18af71e91f6.json"},
         command='python3 merger.py',
-        auto_remove=True,  
-        
+        auto_remove=True,
     )
 
-    t1>>t2>>t3
+    t1 >> t2 >> t3
