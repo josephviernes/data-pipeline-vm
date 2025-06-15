ae7f11f6067969d128b3bf2add15a7f9cfa339bf
joseph8781 <josephnviernes@gmail.com>
2025-06-15 17:11:12 +0800

    added variables for google creds

--- airflow/dags/data_pipeline_dags.py
+++ airflow/dags/data_pipeline_dags.py
@@ -3,6 +3,9 @@ from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime, timedelta
 from docker.types import Mount
 
+creds_container_path = "/gsa/finalproject-456408-a18af71e91f6.json"
+creds_host_folder_path = "/home/joseph/Documents/dez_final_project/gsa"
+
 default_args = {
     'owner': 'jnv',
     'retries': 2,
@@ -25,9 +28,9 @@ with DAG(
         network_mode='bridge',
         mounts=[
             Mount(source="/var/run", target="/var/run", type="bind"),
-            Mount(source="/home/joseph/Documents/dez_final_project/gsa", target="/gsa", type="bind", read_only=True)
+            Mount(source=creds_host_folder_path, target="/gsa", type="bind", read_only=True)
         ],
-        environment={"GOOGLE_APPLICATION_CREDENTIALS": "/gsa/finalproject-456408-a18af71e91f6.json"},
+        environment={"GOOGLE_APPLICATION_CREDENTIALS": creds_container_path},
         command='python3 scraper.py',
         auto_remove=True,
     )
@@ -39,9 +42,9 @@ with DAG(
         network_mode='bridge',
         mounts=[
             Mount(source="/var/run", target="/var/run", type="bind"),
-            Mount(source="/home/joseph/Documents/dez_final_project/gsa", target="/gsa", type="bind", read_only=True)
+            Mount(source=creds_host_folder_path, target="/gsa", type="bind", read_only=True)
         ],
-        environment={"GOOGLE_APPLICATION_CREDENTIALS": "/gsa/finalproject-456408-a18af71e91f6.json"},
+        environment={"GOOGLE_APPLICATION_CREDENTIALS": creds_container_path},
         command='python3 processor.py',
         auto_remove=True,
     )
@@ -53,9 +56,9 @@ with DAG(
         network_mode='bridge',
         mounts=[
             Mount(source="/var/run", target="/var/run", type="bind"),
-            Mount(source="/home/joseph/Documents/dez_final_project/gsa", target="/gsa", type="bind", read_only=True)
+            Mount(source=creds_host_folder_path, target="/gsa", type="bind", read_only=True)
         ],
-        environment={"GOOGLE_APPLICATION_CREDENTIALS": "/gsa/finalproject-456408-a18af71e91f6.json"},
+        environment={"GOOGLE_APPLICATION_CREDENTIALS": creds_container_path},
         command='python3 merger.py',
         auto_remove=True,
     )
