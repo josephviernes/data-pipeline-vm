e231b167d6aac4116744b5107dc16526538fc130
joseph8781 <josephnviernes@gmail.com>
2025-07-06 13:53:55 +0800

    added a table in big query; phivolcs_earthquake table

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,3 +34,58 @@ resource "google_bigquery_dataset" "earthquake_data_dataset" {
   dataset_id = "earthquake_etl_dataset"
   location = "asia-southeast1"
 }
+
+
+resource "google_bigquery_table" "earthquake_table" {
+  dataset_id = google_bigquery_dataset.earthquake_data_dataset.dataset_id
+  table_id   = "phivolcs_earthquake"
+  project    = "earthquake-etl"
+
+  schema = jsonencode([
+    {
+      name = "date_time"
+      type = "TIMESTAMP"
+      mode = "NULLABLE"
+    },
+    {
+      name = "latitude"
+      type = "FLOAT"
+      mode = "NULLABLE"
+    },
+    {
+      name = "longitude"
+      type = "FLOAT"
+      mode = "NULLABLE"
+    },
+    {
+      name = "depth"
+      type = "INTEGER"
+      mode = "NULLABLE"
+    },
+    {
+      name = "magnitude"
+      type = "FLOAT"
+      mode = "NULLABLE"
+    },
+    {
+      name = "relative_location"
+      type = "STRING"
+      mode = "NULLABLE"
+    },
+    {
+      name = "province"
+      type = "STRING"
+      mode = "NULLABLE"
+    },
+    {
+      name = "id"
+      type = "INTEGER"
+      mode = "NULLABLE"
+    },
+    {
+      name = "location_point"
+      type = "GEOGRAPHY"
+      mode = "NULLABLE"
+    }
+  ])
+}
