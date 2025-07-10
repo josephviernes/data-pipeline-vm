ac2f83319acacef9a63203a942502c557a4709fb
joseph8781 <josephnviernes@gmail.com>
2025-07-10 12:48:04 +0800

    added project and dataset environment variable

--- etl/merge/merger.py
+++ etl/merge/merger.py
@@ -4,12 +4,15 @@
 import os
 from google.cloud import bigquery
 
+project = os.environ.get("project")
+dataset = os.environ.get("dataset")
+
 # trigger the big query PROCEDURE (update_earthquake) - put a unique id to temp_table as well as merge it to the main table
 # Set up authentication (assuming ADC is used)
 client = bigquery.Client()
 
 # SQL query to call the stored procedure
-query = "CALL `finalproject-456408.earthquake_dataset.update_earthquakes`()"
+query = f"CALL `{project}.{dataset}.update_earthquakes`()"
 # Execute the query
 client.query(query).result() 
 
